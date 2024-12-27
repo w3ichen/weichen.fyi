8d269bce410e1856d669c82f70d8b4b0a79a350d
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2024-12-27 10:34:56 -0700

    use output export, npm run export is deprecrecated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Generate static HTML files on build
 };
 
 export default nextConfig;
