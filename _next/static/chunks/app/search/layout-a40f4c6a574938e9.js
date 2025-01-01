0a2ddbbfc701ae623f9d56aecbca426d1d9e02aa
w3ichen <w3ichen@users.noreply.github.com>
2025-01-01 04:40:30 +0000

    deploy: a0bb5558de199b1bd871a4488d168f1be648e08a

--- _next/static/chunks/app/search/layout-a40f4c6a574938e9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[244,602,891,361,434,943,155,803],{63521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
--- _next/static/chunks/app/ualberta/layout-79eee1312f97c6cf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[943,602,891,361,434,244,155,803],{63521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
