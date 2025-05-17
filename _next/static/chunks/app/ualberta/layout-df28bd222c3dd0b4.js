67d8e47b27e2c4a88a4bbf3d894e07eca6ab17c4
w3ichen <w3ichen@users.noreply.github.com>
2025-05-17 22:10:48 +0000

    deploy: 71b14c20e03c8fc5a4ec9c89e0e9a73f3bd84547

--- _next/static/chunks/app/ualberta/layout-df28bd222c3dd0b4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8943,9129,5720,3602,5090,5891,1434,8853,2857,5155,2361,244,803,918,4947,9934],{63521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
